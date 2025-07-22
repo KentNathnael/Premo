a95befb33082cc78ab33014e3821e15b70bdd20a
theodorusyw <theo.yuriputra19@gmail.com>
2025-07-22 22:30:31 +0700

    #

--- Deploy/app.py
+++ Deploy/app.py
@@ -17,40 +17,6 @@ def count():
     data = cursor.fetchone() 
     return render_template('index.html', jumlah=data[0])  
 
-# @app.route('/')
-# def makee():
-#     cursor.execute("SELECT DISTINCT make FROM car") 
-#     data = cursor.fetchall()
-#     return render_template('pages/forms/basic_elements.html', jumlah=data)
-
-@app.route('/tambah', methods=['GET', 'POST'])
-def tambah():
-    if request.method == 'POST':
-        nama = request.form['nama']
-        email = request.form['email']
-        cursor.execute("INSERT INTO mahasiswa (nama, email) VALUES (%s, %s)", (nama, email))
-        conn.commit()
-        return redirect('/')
-    return render_template('tambah.html')
-
-@app.route('/edit/<int:id>', methods=['GET', 'POST'])
-def edit(id):
-    if request.method == 'POST':
-        nama = request.form['nama']
-        email = request.form['email']
-        cursor.execute("UPDATE mahasiswa SET nama=%s, email=%s WHERE id=%s", (nama, email, id))
-        conn.commit()
-        return redirect('/')
-    cursor.execute("SELECT * FROM mahasiswa WHERE id=%s", (id,))
-    data = cursor.fetchone()
-    return render_template('edit.html', mhs=data)
-
-@app.route('/hapus/<int:id>')
-def hapus(id):
-    cursor.execute("DELETE FROM mahasiswa WHERE id=%s", (id,))
-    conn.commit()
-    return redirect('/')
-
 @app.route("/pages/ui-features/buttons.html")
 def buttons_page():
     return render_template("pages/ui-features/buttons.html")
@@ -76,10 +42,12 @@ def forms_page():
     selected_trim = None
     selected_year = None
     selected_interior = None
+    selected_sales_year = None
     models = []
     trims = []
     years = []
     interior = []
+    sales_years = []
 
     if request.method == 'POST':
         selected_make = request.form.get('make')
@@ -87,6 +55,7 @@ def forms_page():
         selected_trim = request.form.get('trim')
         selected_year = request.form.get('year')
         selected_interior = request.form.get('interior')
+        selected_sales_year = request.form.get('SaleDate')
 
         if selected_make:
             cursor.execute("SELECT DISTINCT model FROM car WHERE make=%s", (selected_make,))
@@ -103,6 +72,10 @@ def forms_page():
         if selected_make and selected_model and selected_trim and selected_year:
             cursor.execute("SELECT DISTINCT interior FROM car WHERE make=%s AND model=%s AND trim=%s AND year=%s", (selected_make, selected_model, selected_trim, selected_year))
             interior = cursor.fetchall()
+            
+        if selected_make and selected_model and selected_trim and selected_year and selected_interior:
+            cursor.execute("SELECT DISTINCT SaleDate FROM car WHERE make=%s AND model=%s AND trim=%s AND year=%s AND interior=%s", (selected_make, selected_model, selected_trim, selected_year, selected_interior))
+            sales_years = cursor.fetchall()
 
     return render_template(
         'pages/forms/basic_elements.html',
@@ -111,14 +84,15 @@ def forms_page():
         trims=trims,
         years=years,
         interior=interior,
+        sales_years=sales_years,
         conditions=conditions,
         selected_make=selected_make,
         selected_model=selected_model,
         selected_trim=selected_trim,
         selected_year=selected_year,
-        selected_interior = selected_interior
+        selected_interior = selected_interior,
+        selected_sales_year=selected_sales_year
     )
-
 
 
 if __name__ == '__main__':
